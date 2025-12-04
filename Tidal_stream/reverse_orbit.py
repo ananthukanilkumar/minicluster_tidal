c4a78e0062bebb7251a14058015763dcf7f5b192
ananthukanilkumar <ananthukanilkumar@gmail.com>
2025-12-04 13:20:24 +1100

    Update reverse_orbit.py

---
+++ Tidal_stream/reverse_orbit.py
@@ -0,0 +1,13 @@
+import os, numpy as np, agama, scipy.special, scipy.integrate, matplotlib.pyplot as plt
+
+agama.setUnits(length=1, velocity=1, mass=1) #kpc,km/s, Solar mass
+
+def reverse_orbit(ic,pot,time,timestart,show_start=False):
+    orbit_val=agama.orbit(ic=ic, potential=pot, time=-time, timestart=timestart, trajsize=1,accuracy=1e-10)
+    times, trajectory = orbit_val
+    if show_start:
+        print("Starting position:", trajectory[-1])
+    return trajectory[-1]
+
+
+
